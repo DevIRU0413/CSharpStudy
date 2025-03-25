56d4d26fbe17c0b0fa40a8455b0953434b786ffc
[200~ <deviru0413@gmail.com>
2025-03-25 16:15:42 +0900

    출력 공부 정리
    - WriteLine & Write 정리
    - 자리표시자 정리
    
    https://velog.io/@deviru0413/%EC%96%B8%EC%96%B4%EC%9D%98-%EC%8B%9C%EC%9E%91-Hello-World-%EC%B6%9C%EB%A0%A5
    
    https://velog.io/@deviru0413/%EC%96%B8%EC%96%B4%EC%9D%98-%EC%8B%9C%EC%9E%91-Hello-World-%EC%B6%9C%EB%A0%A52

---
+++ IRUCSharpStudy/Print/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 문자열 줄 바꿈 출력
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("\n");
+
+            // 문자열 줄 바꿈 2줄 출력
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("\n");
+
+            // Error!!!
+            // WriteLine("Hello World!");
+
+            // 문자열 줄 바꿈 없이 2줄 출력
+            Console.Write("Hello World!");
+            Console.Write("Hello World!");
+
+            Console.WriteLine("\n");
+
+            // 문자열 줄 바꿈 없음과 있음 복합 출력
+            Console.Write("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.Write("Hello World!");
+
+            Console.WriteLine("\n");
+
+            // 자리표시자 출력 방법
+            Console.WriteLine("Hello {0}!", "World"); // 1
+            Console.WriteLine("{0} {1}!", "Hello", "World"); // 2
+            Console.WriteLine("{0} {0}!", "Hello", "World"); // 3
+            Console.WriteLine("{1} {1}!", "Hello", "World"); // 4
+            Console.WriteLine("{0} {1}!", 1, 2); // 5
+            Console.WriteLine("{0} {1}", 1, 2); // 6
+            Console.WriteLine("{0} {1}", "1", "2"); // 7
+            Console.WriteLine("{0} {1}", 2, 2); // 8
+            Console.WriteLine("{0} {1}", 10 + 1, 10 - 1); // 9
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}, {6}...", "안", "녕", "하", "세", "요", "~", "~~!!!"); // 9
+            Console.WriteLine("{0}, {0}, {1}, {1}, {1}, {5}, {1}...", "안", "녕", "하", "세", "요", "~", "~~!!!");
+        }
+    }
+}
